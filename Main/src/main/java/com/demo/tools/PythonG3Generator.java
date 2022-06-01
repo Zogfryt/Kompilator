0493604732b75b87f0c14a55ac314e80d727eba6
Zogfryt <72657015+Zogfryt@users.noreply.github.com>
2022-06-01 19:13:15 +0200

    Naprawione wszystko, dodanie visitora

--- Main/src/main/java/com/demo/tools/PythonG3Generator.java
+++ Main/src/main/java/com/demo/tools/PythonG3Generator.java
@@ -1,32 +1,34 @@
 package com.demo.tools;
 
-import com.demo.lexerAndParser.PythonG3BaseListener;
+import com.demo.lexerAndParser.PythonG3BaseVisitor;
 import com.demo.lexerAndParser.PythonG3Parser;
 
 import java.io.*;
 import java.util.HashMap;
 
-public class PythonG3Generator extends PythonG3BaseListener {
+public class PythonG3Generator extends PythonG3BaseVisitor<String> {
     private PrintWriter writer;
+    private HashMap<String,Type> values;
 
 
     public PythonG3Generator(String path) throws IOException {
         FileManager manager = new FileManager();
         writer = manager.createPrintWriterToGivenFile(path);
+        values = new HashMap<>();
     }
 
     @Override
-    public void enterFile_input(PythonG3Parser.File_inputContext ctx)
+    public String visitFile_input(PythonG3Parser.File_inputContext ctx)
     {
         writer.println("int main() {");
-    }
-
-    @Override
-    public void exitFile_input(PythonG3Parser.File_inputContext ctx)
-    {
+        for(var child : ctx.children)
+        {
+            visit(child);
+        }
         writer.println("return 0;");
         writer.println("}");
         writer.close();
+        return null;
     }
 
 }
